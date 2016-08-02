c47c3215950323cca999cf4b10f36c16e8c4c9c4
Guyren Howe <guyren@relevantlogic.com>
2016-08-01 22:45:00 -0700

    Stub method definitions for relations

---
+++ lib/frest/relation.rb
@@ -0,0 +1,42 @@
+module Frest
+  module Relation
+    def create_relation(
+      name:,
+      fields: []
+    )
+
+
+    end
+
+    def insert_relation(
+      name:,
+      fields: [],
+      values: []
+    )
+
+    end
+
+    def find_relation(
+      name:,
+      where: {},
+      fields: []
+    )
+
+    end
+
+    def update_relation(
+      name:,
+      where: {},
+      values: {}
+    )
+
+    end
+
+    def delete_relation(
+      name:,
+      where: {}
+    )
+
+    end
+  end
+end
